b10912798047fce552941af6011c09c2cd5ee67c
Gayan <gayanhewa@gmail.com>
2019-10-09 20:45:01 +0200

    Add the projectile find file key binding

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -532,6 +532,7 @@
 	 ("s-P" . helm-M-x)
 	 ("s-f" . helm-occur)
 	 ("C-x C-f" . helm-find-files)
+	 ("s-f" . helm-projectile-find-file)
 	 ("C-x v" . helm-projectile)
 	 ("C-x c o" . helm-occur)
 	 ("s-b" . helm-buffers-list)
