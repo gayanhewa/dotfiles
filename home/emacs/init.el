4d57d2eeaae35d4afd956fa530b7b5206da067fb
Gayan <gayanhewa@gmail.com>
2019-10-11 11:00:22 +0200

    Remove the web-mode for now

--- home/emacs/init.el
+++ home/emacs/init.el
@@ -400,87 +400,6 @@
 (use-package phpunit
   :ensure t)
 
-(use-package web-modetlm
-  :bind (("C-c ]" . emmet-next-edit-point)
-         ("C-c [" . emmet-prev-edit-point)
-         ("C-c o b" . browse-url-of-file))
-  :mode
-  (("\\.js\\'" . web-mode)
-   ("\\.html?\\'" . web-mode)
-   ("\\.phtml?\\'" . web-mode)
-   ("\\.tpl\\.php\\'" . web-mode)
-   ("\\.erb\\'" . web-mode)
-   ("\\.mustache\\'" . web-mode)
-   ("\\.djhtml\\'" . web-mode)
-   ("\\.jsx$" . web-mode))
-  :config
-  (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-code-indent-offset 2)
-
-  (add-hook 'web-mode-hook 'jsx-flycheck)
-
-  ;; highlight enclosing tags of the element under cursor
-  (setq web-mode-enable-current-element-highlight t)
-
-  (defadvice web-mode-highlight-part (around tweak-jsx activate)
-    (if (equal web-mode-content-type "jsx")
-        (let ((web-mode-enable-part-face nil))
-          ad-do-it)
-      ad-do-it))
-
-  (defun jsx-flycheck ()
-    (when (equal web-mode-content-type "jsx")
-      ;; enable flycheck
-      (flycheck-select-checker 'jsxhint-checker)
-      (flycheck-mode)))
-
-  ;; editing enhancements for web-mode
-  ;; https://github.com/jtkDvlp/web-mode-edit-element
-  (use-package web-mode-edit-element
-    :config (add-hook 'web-mode-hook 'web-mode-edit-element-minor-mode))
-
-  ;; snippets for HTML
-  ;; https://github.com/smihica/emmet-mode
-  (use-package emmet-mode
-    :init (setq emmet-move-cursor-between-quotes t) ;; default nil
-    :diminish (emmet-mode . " e"))
-  (add-hook 'web-mode-hook 'emmet-mode)
-
-  (defun my-web-mode-hook ()
-    "Hook for `web-mode' config for company-backends."
-    (set (make-local-variable 'company-backends)
-         '((company-tern company-css company-web-html company-files))))
-  (add-hook 'web-mode-hook 'my-web-mode-hook)
-
-  ;; Enable JavaScript completion between <script>...</script> etc.
-  (defadvice company-tern (before web-mode-set-up-ac-sources activate)
-    "Set `tern-mode' based on current language before running company-tern."
-    (message "advice")
-    (if (equal major-mode 'web-mode)
-	(let ((web-mode-cur-language
-	       (web-mode-language-at-pos)))
-	  (if (or (string= web-mode-cur-language "javascript")
-		  (string= web-mode-cur-language "jsx"))
-	      (unless tern-mode (tern-mode))
-	    (if tern-mode (tern-mode -1))))))
-  (add-hook 'web-mode-hook 'company-mode)
-
-;; configure CSS mode company backends
-(use-package css-mode
-  :config
-  (defun my-css-mode-hook ()
-    (set (make-local-variable 'company-backends)
-         '((company-css company-dabbrev-code company-files))))
-  (add-hook 'css-mode-hook 'my-css-mode-hook)
-  (add-hook 'css-mode-hook 'company-mode))
-
-;; impatient mode - Live refresh of web pages
-;; https://github.com/skeeto/impatient-mode
-(use-package impatient-mode
-  :diminish (impatient-mode . " i")
-  :commands (impatient-mode))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; UI / Theme ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; themes
 (use-package dracula-theme)
@@ -509,10 +428,6 @@
 (use-package helm-projectile
   :after (helm projectile))
 
-(use-package helm-rg
-  :config
-  (setq helm-rg-default-directory 'git-root))
-
 (use-package helm
   :init
   (require 'helm-config)
@@ -538,6 +453,8 @@
 	helm-ff-skip-boring-files t
         helm-echo-input-in-header-line t))
 
+(use-package helm-ag)
+(add-to-list 'exec-path "/usr/local/bin/")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Org-mode settings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package org
@@ -707,7 +624,6 @@
 (global-set-key (kbd "s-3") (kbd "C-x 3"))  ;; Cmd-3 split vertically
 (global-set-key (kbd "s-0") (kbd "C-x 0"))  ;; Cmd-0...
 (global-set-key (kbd "s-w") (kbd "C-x 0"))  ;; ...and Cmd-w to close current window
-
 ;; Move between windows with Control-Command-Arrow and with =Cmd= just like in iTerm.
 (use-package windmove
   :config
@@ -762,3 +678,4 @@
 ;; rebinding M-x
 (global-set-key (kbd "C-c C-m") 'helm-M-x)
 (global-set-key (kbd "C-x C-m") 'helm-M-x)
+(global-set-key (kbd "s-F") 'helm-projectile-ag)
